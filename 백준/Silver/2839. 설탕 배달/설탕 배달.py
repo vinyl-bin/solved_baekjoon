e240590b5cd4db23c48903a4ada626a8003a1129
Dabin Kang <85878793+vinyl-bin@users.noreply.github.com>
2022-12-22 21:13:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,12 @@
+sugar = int(input())
+bag = 0
+
+while sugar >= 0:
+    if sugar % 5 == 0:
+        bag += sugar // 5
+        print(bag)
+        break
+    sugar -= 3
+    bag += 1
+if sugar < 0:
+    print(-1)
