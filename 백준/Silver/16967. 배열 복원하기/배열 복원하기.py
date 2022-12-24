45def5e1ccd7ddf5a5160a5a2f75d08c7824037e
Dabin Kang <85878793+vinyl-bin@users.noreply.github.com>
2022-12-24 18:34:58 +0900

    [Silver III] Title: 배열 복원하기, Time: 180 ms, Memory: 38080 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -1,11 +1,11 @@
 H, W, X, Y = map(int, input().split())
 
-B_array = [0 * (W + Y) for _ in range(H + X)]
+B_array = [[0] * (W + Y) for _ in range(H + X)]
 
 for i in range(H + X):
     B_array[i] = list(map(int, input().split()))
 
-A_array = [[0] * W for _ in range(H)] #왜 [0]을 해야하는지?
+A_array = [[0] * W for _ in range(H)]  # 왜 [0]을 해야하는지?
 
 for i in range(H):
     for j in range(W):
@@ -19,3 +19,5 @@ for i in range(H):
     for j in range(W):
         print(A_array[i][j], end=" ")
     print("")
+
+# 참고 https://velog.io/@sugenius77/%EB%B0%B1%EC%A4%80Python-16967%EB%B2%88-%EB%B0%B0%EC%97%B4-%EB%B3%B5%EC%9B%90%ED%95%98%EA%B8%B0
